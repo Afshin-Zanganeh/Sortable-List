db26e2b13bfc52d416afcb2dfa36679fc6581785
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2021-08-20 10:37:01 +0430

    Add drag and drop functionality and some styles related to it

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ correctOrder
   .sort((a, b) => a.value - b.value)
   .map((elem) => elem.name)
   .forEach((person, index) => {
-    draggableList.innerHTML += `<div class="draggable-item"><div class="number">${
+    draggableList.innerHTML += `<div draggable="true" class="draggable-item"><div class="number">${
       index + 1
     }</div><p class="name">${person}</p><i class="fas fa-bars"></i></div>`;
   });
@@ -25,11 +25,37 @@ checkBtn.addEventListener("click", () => {
   const items = document.querySelectorAll(".name");
   items.forEach((item, index) => {
     if (item.innerText === correctOrder[index]) {
-        item.classList.remove('wrong')
-        item.classList.add('correct')
-    }else {
-        item.classList.remove('correct')
-        item.classList.add('wrong')
+      item.classList.remove("wrong");
+      item.classList.add("correct");
+    } else {
+      item.classList.remove("correct");
+      item.classList.add("wrong");
     }
+  });
+});
+
+const draggables = document.querySelectorAll(".draggable-item");
+let draggedItem;
+draggables.forEach((item) => {
+  item.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("text", item.querySelector(".name").innerText);
+    draggedItem = item;
+  });
+  item.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+  item.addEventListener("dragenter", () => {
+    item.classList.add("over");
+  });
+  item.addEventListener("dragleave", () => {
+    item.classList.remove("over");
+  });
+  item.addEventListener("drop", (e) => {
+    e.preventDefault();
+    item.classList.remove("over");
+    const temp = e.dataTransfer.getData("text");
+    const name = item.querySelector(".name");
+    draggedItem.querySelector(".name").innerText = name.innerText;
+    name.innerText = temp;
   });
 });
